79040f95c058abc530fcf939857d091fb12c5f35
ZhilPro27 <zhilalkrisna@gmail.com>
2024-05-13 15:50:48 +0700

    Mendefinisikan method dan membuat object

--- constructor.cpp
+++ constructor.cpp
@@ -14,7 +14,7 @@ class Mahasiswa{
     Mahasiswa(int iNim){
         nim = iNim;
     };
-    Mahasiswa(string);
+    Mahasiswa(string iNama);
     Mahasiswa(int iNim, string iNama);
     void cetak(){
         cout << "NIM    = " << nim << endl;
@@ -22,3 +22,25 @@ class Mahasiswa{
         cout << endl;
     };
 };
+
+Mahasiswa::Mahasiswa(string iNama){
+    nim = 0;
+    nama = iNama;
+}
+
+Mahasiswa::Mahasiswa(int iNim, string iNama){
+    nim = iNim;
+    nama = iNama;
+}
+
+int main(){
+    Mahasiswa mhs1;
+    Mahasiswa mhs2(123);
+    Mahasiswa mhs3("Zhilal");
+    Mahasiswa mhs4(27, "Krisna");
+
+    mhs1.cetak();
+    mhs2.cetak();
+    mhs3.cetak();
+    mhs4.cetak();
+}

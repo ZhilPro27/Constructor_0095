6554edd77be11d60e0868f9e0f468ce4bd2b0aa2
ZhilPro27 <zhilalkrisna@gmail.com>
2024-05-13 14:37:24 +0700

    Membuat member di dalam class mahasiswa

--- mbrStatik.cpp
+++ mbrStatik.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 
 class mahasiswa {
+    public:
+        static int nim;
+        int id;
+        string nama;
 
+        void setID();
+
+        void printAll();
+        mahasiswa(string pnama) :nama(pnama){
+            setID();
+        }
 };

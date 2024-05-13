4c79968398fa07f75b53b710d3e10e0652962232
ZhilPro27 <zhilalkrisna@gmail.com>
2024-05-13 15:12:13 +0700

    Mendefinisikan method dan membuat object

--- mbrStatikfunc.cpp
+++ mbrStatikfunc.cpp
@@ -20,3 +20,31 @@ class Mahasiswa{
             setID();
         }
 };
+
+int Mahasiswa::nim = 100;
+
+void Mahasiswa::setID(){
+    id = ++nim;
+}
+
+void Mahasiswa::printAll(){
+    cout << "ID     = " << id << endl;
+    cout << "Nama   = " << nama << endl;
+    cout << endl;
+}
+
+int main(){
+    Mahasiswa mhs1("Lia Kurnia");
+    Mahasiswa mhs2("Asroni");
+
+    int Mahasiswa::nim = 9;
+    Mahasiswa mhs3("Andi Kurniawan");
+    Mahasiswa mhs4("Joko Purbo");
+
+    mhs1.printAll();
+    mhs2.printAll();
+    mhs3.printAll();
+    mhs4.printAll();
+
+    return 0;
+}
